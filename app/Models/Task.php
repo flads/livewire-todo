169218653845a9a377b6affa7120a51c78804077
flads <flads.me@gmail.com>
2021-01-24 16:23:09 -0300

    add soft delete to tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
+    use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
